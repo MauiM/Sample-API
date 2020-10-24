753f4577e0c342a1810418e52ea786db3e1e7dab
Maui <v-mamcad@microsoft.com>
2020-10-24 09:20:29 -0700

    Initial Commit

---
+++ Sample.API/Models/JsonPayload.cs
@@ -0,0 +1,23 @@
+﻿namespace Sample.API.Models
+{
+    public class JsonResult<T>
+    {
+        public T Data { get; set; }
+
+        public bool IsSuccess
+        {
+            get
+            {
+                return string.IsNullOrWhiteSpace(Error);
+            }
+
+        }
+
+        public string Error { get; set; } = string.Empty;
+
+        public JsonResult(T data)
+        {
+            Data = data;
+        }
+    }
+}
